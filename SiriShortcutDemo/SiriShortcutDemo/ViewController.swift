56f7278b243ee51e755b5176637fa6fb365c0fe4
Haoxin Li <54lihaoxin@gmail.com>
2018-10-02 04:17:53 -0700

    Create project

---
+++ SiriShortcutDemo/SiriShortcutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SiriShortcutDemo
+//
+//  Created by Haoxin Li on 10/2/18.
+//  Copyright © 2018 Haoxin Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
